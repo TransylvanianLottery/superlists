37671427cd6388b15c66812fd57d2559a684fc97
TransylvanianLottery <cpelish@gmail.com>
2019-02-23 19:44:18 -0500

    new URL + view for adding to existing lists. FT passes!

--- superLists/urls.py
+++ superLists/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
     url(r'^Lists/new$', views.new_list, name='new_list'),
-    url(r'^Lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^Lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^Lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
